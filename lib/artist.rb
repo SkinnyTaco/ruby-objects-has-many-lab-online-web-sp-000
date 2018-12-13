9f7af1c68c54a8bd0f13311a2f0a3b320f7e6feb
Daniel Dawson <dannyldawson@gmail.com>
2018-12-13 06:58:36 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,12 @@
+class Artist 
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+  end
+  
+end
+
+# Please let this comment make the submitting glitch stop
+# Seriously, stop...
---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song 
+  
+  attr_accessor :name, :artist
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def self.all
+    self.all
+  end
+  
+  def artist_name
+    self.artist_name
+  end
+  
+end
